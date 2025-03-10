66164f4d36cb80ff865331a572fc8db590606ca0
Abhay D <a.deshpande012@gmail.com>
2025-03-09 23:26:14 -0700

    Add aws secret to run

--- scripts/launch_scene_gen.sh
+++ scripts/launch_scene_gen.sh
@@ -1,2 +1,6 @@
-gantry run --workspace ai2/abhayd --budget ai2/prior --dataset abhayd/acronym_processed:/data -- \
+gantry run --workspace ai2/abhayd --budget ai2/prior \
+    --dataset abhayd/acronym_processed:/data \
+    --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
+    --env-secret AWS_SECRET_ACCESS_KEY=AWS_ACCESS_KEY \
+    -- \
     python semantic_grasping_datagen/datagen/datagen.py
