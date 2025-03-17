58b33fb78a43011fa8cf0e2183c2f7bc56288993
Abhay D <a.deshpande012@gmail.com>
2025-03-17 14:53:30 -0700

    Update obs gen launch

--- scripts/launch_obs_gen.sh
+++ scripts/launch_obs_gen.sh
@@ -2,5 +2,9 @@ gantry run --budget ai2/prior -w ai2/abhayd \
     --dataset abhayd/datagen_0310_1527:/data \
     --gpus 1 \
     --beaker-image ai2/cuda11.8-ubuntu20.04 \
+    --priority normal \
+    --cluster ai2/prior-elanding \
+    --cluster ai2/ceres-cirrascale \
+    --cluster ai2/jupiter-cirrascale-2 \
     -- \
     python semantic_grasping_datagen/datagen/generate_obs.py
