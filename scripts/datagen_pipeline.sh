490e579839d396fd06b67089e9a0efe934ab6645
Abhay D <a.deshpande012@gmail.com>
2025-04-07 17:30:04 -0700

    Update datagen to use weka

--- scripts/datagen_pipeline.sh
+++ scripts/datagen_pipeline.sh
@@ -1,4 +1,5 @@
-python semantic_grasping_datagen/datagen/datagen.py out_dir=/results/scenes
-python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=/results/scenes out_dir=/results/observations
-python semantic_grasping_datagen/datagen/collate_data.py /results/observations /results --annot-type full
-rm -r /results/scenes  # delete original scenes to save space
+DATASET_PATH=/data/abhayd/semantic-grasping-datasets/0407_1606
+ASSETS_PATH=/data/abhayd/semantic-grasping-datasets/acronym_processed
+python semantic_grasping_datagen/datagen/datagen.py out_dir=${DATASET_PATH}/scenes data_dir=${ASSETS_PATH}
+python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
+python semantic_grasping_datagen/datagen/collate_data.py ${DATASET_PATH}/observations ${DATASET_PATH} --annot-type full
--- scripts/launch_datagen.sh
+++ scripts/launch_datagen.sh
@@ -1,16 +1,15 @@
 gantry run --budget ai2/prior -w ai2/abhayd \
-    --dataset abhayd/acronym_processed:/assets \
     --dataset abhayd/synthetic_annotations_0401_1426:/annotations \
+    --weka prior-default:/data \
     --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
     --env-secret AWS_SECRET_ACCESS_KEY=AWS_ACCESS_KEY \
     --gpus 1 \
     --beaker-image ai2/cuda11.8-ubuntu20.04 \
     --priority normal \
-    --cluster ai2/augusta-google-1 \
     --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ai2/saturn-cirrascale \
     --cluster ai2/neptune-cirrascale \
     --cluster ai2/ceres-cirrascale \
-    --cluster ai2/prior-elanding \
     --allow-dirty \
     -- \
     /bin/bash scripts/datagen_pipeline.sh
