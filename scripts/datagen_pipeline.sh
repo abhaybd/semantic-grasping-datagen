675bf15cc697a41069ca9ea4bad089146d0412ca
Abhay D <a.deshpande012@gmail.com>
2025-04-11 01:42:13 -0700

    update datagen script

--- scripts/datagen_pipeline.sh
+++ scripts/datagen_pipeline.sh
@@ -1,5 +1,12 @@
-DATASET_PATH=/data/abhayd/semantic-grasping-datasets/0407_1606
+#!/bin/bash
+
+set -euxo pipefail
+
+DATASET_PATH=/data/abhayd/semantic-grasping-datasets/0410_2243
 ASSETS_PATH=/data/abhayd/semantic-grasping-datasets/acronym_processed
-python semantic_grasping_datagen/datagen/datagen.py out_dir=${DATASET_PATH}/scenes data_dir=${ASSETS_PATH}
+python semantic_grasping_datagen/datagen/datagen.py \
+    out_dir=${DATASET_PATH}/scenes \
+    data_dir=${ASSETS_PATH} \
+    "annotation_sources=[{type:directory,params:{dir:${DATASET_PATH}/annotations}}]"
 python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
 python semantic_grasping_datagen/datagen/collate_data.py ${DATASET_PATH}/observations ${DATASET_PATH} --annot-type full
--- scripts/launch_datagen.sh
+++ scripts/launch_datagen.sh
@@ -1,11 +1,10 @@
 gantry run --budget ai2/prior -w ai2/abhayd \
-    --dataset abhayd/synthetic_annotations_0401_1426:/annotations \
     --weka prior-default:/data \
     --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
     --env-secret AWS_SECRET_ACCESS_KEY=AWS_ACCESS_KEY \
     --gpus 1 \
     --beaker-image ai2/cuda11.8-ubuntu20.04 \
-    --priority normal \
+    --priority high \
     --cluster ai2/jupiter-cirrascale-2 \
     --cluster ai2/saturn-cirrascale \
     --cluster ai2/neptune-cirrascale \
