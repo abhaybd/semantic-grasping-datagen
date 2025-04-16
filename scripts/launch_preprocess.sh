37c8537707582b36f0de17cbce632ce7f0bac9ac
Abhay D <a.deshpande012@gmail.com>
2025-04-15 22:23:40 -0700

    Update launch preprocess script

--- scripts/launch_preprocess.sh
+++ scripts/launch_preprocess.sh
@@ -1,3 +1,16 @@
-gantry run --dataset abhayd/acronym:/acronym --budget ai2/prior -w ai2/abhayd -- \
-    python semantic_grasping_datagen/preprocess_shapenet.py /acronym/grasps /acronym/ShapeNetSem /results \
-    --blacklist asset_blacklist.txt --sampling-categories-file all_categories.txt
+gantry run --budget ai2/prior -w ai2/abhayd \
+    --weka prior-default:/data \
+    --priority high \
+    --cluster ai2/jupiter-cirrascale-2 \
+    --cluster ai2/saturn-cirrascale \
+    --cluster ai2/neptune-cirrascale \
+    --cluster ai2/ceres-cirrascale \
+    --allow-dirty \
+    -- \
+    python semantic_grasping_datagen/preprocess_shapenet.py \
+        /data/abhayd/semantic-grasping-datasets/acronym/grasps \
+        /data/abhayd/semantic-grasping-datasets/acronym/ShapeNetSem \
+        /data/abhayd/semantic-grasping-datasets/acronym_processed \
+        --blacklist asset_blacklist.txt \
+        --sampling-categories-file all_categories.txt \
+        --step project
