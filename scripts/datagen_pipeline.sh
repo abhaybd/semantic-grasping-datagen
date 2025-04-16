7d36e19653a2e1f6a933a84939b18cc91d78a90b
Abhay D <a.deshpande012@gmail.com>
2025-04-16 00:24:51 -0700

    tmp

--- scripts/datagen_pipeline.sh
+++ scripts/datagen_pipeline.sh
@@ -5,10 +5,10 @@ set -euxo pipefail
 DATASET_PATH=/data/abhayd/semantic-grasping-datasets/0415_2226_small_2
 ASSETS_PATH=/data/abhayd/semantic-grasping-datasets/acronym_processed
 ANNOTS_PATH=/data/abhayd/semantic-grasping-datasets/0410_2243/annotations
-python semantic_grasping_datagen/datagen/datagen.py \
-    out_dir=${DATASET_PATH}/scenes \
-    data_dir=${ASSETS_PATH} \
-    n_samples=100 \
-    "annotation_sources=[{type:directory,params:{dir:${ANNOTS_PATH}}}]"
+# python semantic_grasping_datagen/datagen/datagen.py \
+#     out_dir=${DATASET_PATH}/scenes \
+#     data_dir=${ASSETS_PATH} \
+#     n_samples=100 \
+#     "annotation_sources=[{type:directory,params:{dir:${ANNOTS_PATH}}}]"
 python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
 python semantic_grasping_datagen/datagen/collate_data.py ${DATASET_PATH}/observations ${DATASET_PATH} --annot-type full
--- scripts/launch_datagen.sh
+++ scripts/launch_datagen.sh
@@ -5,10 +5,7 @@ gantry run --budget ai2/prior -w ai2/abhayd \
     --gpus 1 \
     --beaker-image ai2/cuda11.8-ubuntu20.04 \
     --priority high \
-    --cluster ai2/jupiter-cirrascale-2 \
-    --cluster ai2/saturn-cirrascale \
-    --cluster ai2/neptune-cirrascale \
-    --cluster ai2/ceres-cirrascale \
+    --hostname jupiter-cs-aus-177.reviz.ai2.in \
     --allow-dirty \
     -- \
     /bin/bash scripts/datagen_pipeline.sh
