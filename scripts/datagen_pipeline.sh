10927c454ada78619be0e2435b8f1c91cb38c8f8
Abhay D <a.deshpande012@gmail.com>
2025-04-17 22:31:47 -0700

    Add vlm datapackage to datagen pipeline

--- scripts/datagen_pipeline.sh
+++ scripts/datagen_pipeline.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 DATASET_PATH=/data/abhayd/semantic-grasping-datasets/0417_2121
 ASSETS_PATH=/data/abhayd/semantic-grasping-datasets/acronym_processed
 ANNOTS_PATH=/data/abhayd/semantic-grasping-datasets/synthetic_annotations_filtered_0417_2015
+VLM_FORMAT="molmo"
 
 python semantic_grasping_datagen/datagen/datagen.py \
     out_dir=${DATASET_PATH}/scenes \
@@ -13,3 +14,9 @@ python semantic_grasping_datagen/datagen/datagen.py \
     "annotation_sources=[{type:directory,params:{dir:${ANNOTS_PATH}}}]"
 python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
 python semantic_grasping_datagen/datagen/collate_data.py ${DATASET_PATH}/observations ${DATASET_PATH} --annot-type full
+python semantic_grasping_datagen/create_vlm_data.py \
+    ${DATASET_PATH}/${VLM_FORMAT}_data \
+    ${DATASET_PATH}/observations \
+    ${DATASET_PATH}/dataset.csv \
+    --format ${VLM_FORMAT} \
+    --n-proc 32
