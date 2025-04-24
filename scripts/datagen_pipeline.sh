016b0dc46d9eb5bf9e2209ae5be8532e28a310d4
Abhay D <a.deshpande012@gmail.com>
2025-04-23 17:28:07 -0700

    Revert "tmp: revertme"
    This reverts commit 31443db55c6e993ecd76ffd8703b93994e53d9c6.

--- scripts/datagen_pipeline.sh
+++ scripts/datagen_pipeline.sh
@@ -8,45 +8,45 @@ ANNOTS_PATH=/weka/prior/abhayd/semantic-grasping-datasets/synthetic_annotations_
 TASKS_JSON=/weka/prior/abhayd/semantic-grasping-datasets/generated_tasks/0422_1700.json
 FORMAT=molmo
 
-# python semantic_grasping_datagen/datagen/split_data.py \
-#     data_dir=${ASSETS_PATH} \
-#     out_dir=${DATASET_PATH}/splits
+python semantic_grasping_datagen/datagen/split_data.py \
+    data_dir=${ASSETS_PATH} \
+    out_dir=${DATASET_PATH}/splits
 
-# # Train datagen
-# echo "Generating train scenes"
-# python semantic_grasping_datagen/datagen/datagen.py \
-#     out_dir=${DATASET_PATH}/scenes \
-#     data_dir=${ASSETS_PATH} \
-#     split_file=${DATASET_PATH}/splits/train.json \
-#     n_samples=10000 \
-#     "annotation_sources=[{type:directory,params:{dir:${ANNOTS_PATH}}}]"
+# Train datagen
+echo "Generating train scenes"
+python semantic_grasping_datagen/datagen/datagen.py \
+    out_dir=${DATASET_PATH}/scenes \
+    data_dir=${ASSETS_PATH} \
+    split_file=${DATASET_PATH}/splits/train.json \
+    n_samples=10000 \
+    "annotation_sources=[{type:directory,params:{dir:${ANNOTS_PATH}}}]"
 
-# echo "Generating train observations"
-# python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
+echo "Generating train observations"
+python semantic_grasping_datagen/datagen/generate_obs.py scene_dir=${DATASET_PATH}/scenes out_dir=${DATASET_PATH}/observations
 
-# echo "Matching tasks to grasps"
-# python semantic_grasping_datagen/datagen/match_tasks_to_grasps_v2.py \
-#     ${TASKS_JSON} \
-#     ${DATASET_PATH}/observations \
-#     ${DATASET_PATH}/task_point_v2 \
-#     --submit \
-#     --retrieve
+echo "Matching tasks to grasps"
+python semantic_grasping_datagen/datagen/match_tasks_to_grasps_v2.py \
+    ${TASKS_JSON} \
+    ${DATASET_PATH}/observations \
+    ${DATASET_PATH}/task_point_v2 \
+    --submit \
+    --retrieve
 
-# echo "Packaging train data"
-# python semantic_grasping_datagen/datagen/package_pointing_data.py \
-#     ${DATASET_PATH}/task_point_v2/matched_tasks.csv \
-#     ${DATASET_PATH}/observations \
-#     ${DATASET_PATH}/${FORMAT}_data_cot \
-#     --format ${FORMAT} \
-#     --cot \
-#     --n-proc 32
+echo "Packaging train data"
+python semantic_grasping_datagen/datagen/package_pointing_data.py \
+    ${DATASET_PATH}/task_point_v2/matched_tasks.csv \
+    ${DATASET_PATH}/observations \
+    ${DATASET_PATH}/${FORMAT}_data_cot \
+    --format ${FORMAT} \
+    --cot \
+    --n-proc 32
 
-# python semantic_grasping_datagen/datagen/package_pointing_data.py \
-#     ${DATASET_PATH}/task_point_v2/matched_tasks.csv \
-#     ${DATASET_PATH}/observations \
-#     ${DATASET_PATH}/${FORMAT}_data \
-#     --format ${FORMAT} \
-#     --n-proc 32
+python semantic_grasping_datagen/datagen/package_pointing_data.py \
+    ${DATASET_PATH}/task_point_v2/matched_tasks.csv \
+    ${DATASET_PATH}/observations \
+    ${DATASET_PATH}/${FORMAT}_data \
+    --format ${FORMAT} \
+    --n-proc 32
 
 # Test datagen
 echo "Generating test scenes"
